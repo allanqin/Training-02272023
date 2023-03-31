032475df8aff16b8088fbfe08d46ce578268f094
allanqin <allanqin123@gmail.com>
2023-03-31 13:47:03 -0700

    lib -> src

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./lib/index.js",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
